3bd7abbdeac27d9c065fa3959bfee1b6de1cc372
Pedro Bueno <pedro.bueno@edu.unifil.br>
2025-11-07 21:06:23 -0300

    Subtração

--- main.java
+++ main.java
@@ -1,16 +1,19 @@
-public static void main(){
-  algoritmo "SomaSimples"
-var
-   a, b, resultado: inteiro
-inicio
-   escreva("Digite o primeiro número: ")
-   leia(a)
-   escreva("Digite o segundo número: ")
-   leia(b)
-   
-   resultado <- a + b
-   
-   escreva("A soma é: ", resultado)
-fimalgoritmo
+import java.util.Scanner;
 
+public class SubtracaoSimples {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int numero1 = entrada.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int numero2 = entrada.nextInt();
+
+        int resultado = numero1 - numero2;
+
+        System.out.println("O resultado da subtração é: " + resultado);
+
+        entrada.close();
+    }
 }
